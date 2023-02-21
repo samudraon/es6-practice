7beb5839278cdc4daffd2f146517bb1c96d74612
samudraon <samudraon@gmailcom>
2023-02-21 23:16:19 +0600

    ternary operator

---
+++ ternary.js
@@ -0,0 +1,17 @@
+const isStudent = false;
+let x = 7;
+let paragraph = '';
+
+// if (isStudent === true) {
+//     paragraph = 'he is a student';
+// }
+// else {
+//     paragraph = 'porasuna bhallagena';
+// }
+
+// (x === 5) ? console.log('he is a student') : console.log('not student');
+
+/* for two condition use this ternary operator */
+paragraph = (isStudent === true) ? 'he is a student' : 'porasuna bhallagena';
+
+console.log(paragraph);
