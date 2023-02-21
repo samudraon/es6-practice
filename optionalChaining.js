59da041e06be441a84aeb7f9d9db44d126fce42f
samudraon <samudraon@gmailcom>
2023-02-22 01:54:04 +0600

    optional chaining

---
+++ optionalChaining.js
@@ -0,0 +1,16 @@
+const petOwner = {
+    name: 'Jolonto Khalil',
+    contact: {
+        phone: '013452164',
+        email: 'alam@gmail.com',
+    },
+    pet: {
+        name: 'Lofie',
+        info: {
+            color: 'black',
+            weight: '1.2Kg',
+        },
+    },
+};
+
+console.log(petOwner?.dog?.name);
