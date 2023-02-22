5a70c18ee0d0b33b7350691abd86c1e841e360fc
samudraon <samudraon@gmailcom>
2023-02-22 19:49:59 +0600

    find filter added

---
+++ findFilter.js
@@ -0,0 +1,8 @@
+const numbers = [12, 32, 25, 34, 61, 16, 31, 57, 42];
+
+const names = ['babul', 'abul', 'kalam', 'shorif', 'jamal'];
+
+// const x = numbers.find(n => n >= 50);
+
+const x = numbers.filter(n => n >= 50);
+console.log(x);
